8971afb5ad87c1bad671273e5cb5624dc2680211
prrrcl <adrianporcelnavarro@hotmail.com>
2019-09-28 10:26:08 +0200

    edited services

--- client/src/pages/em/index.jsx
+++ client/src/pages/em/index.jsx
@@ -2,10 +2,26 @@
 import React, {useState, useEffect} from 'react'
 import CreateTrip from './../createTrip'
 import withAuth from '../../hoc/withAuth'
+import apiService from '../../services/api-service'
 import io from 'socket.io-client'
 
-const Em = (props) => {
+const socket = io(process.env.REACT_APP_BACKEND_DOMAIN)
 
+const Em = (props) => {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(()=>{
+    socket.emit('me');
+    apiService.getAllTrips()
+    .then(res =>{
+      setTrips(res.data.listOfTrips)
+    })
+  },[]);
+  useEffect(()=>{
+    socket.on('me', tripsfrom => {
+      console.log(tripsfrom, 'he entrado')
+    })
+  })
   useEffect(()=>{
     if(props.user.userType !== 'volunteer'){
       props.history.push('/private');
@@ -15,7 +31,10 @@ const Em = (props) => {
 
   return (
     <>
-      
+      {trips.length >0 ?
+      (<div>si hay </div>)
+      :
+      (<div>no hay </div>)}
     </>
   )
 }
